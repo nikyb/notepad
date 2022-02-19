d6f1ed2bddcca2c5dbf10a909e1b3e6c52cbd2d2
test <test@mail>
2022-02-19 15:54:44 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template> 
+<div>  
+<button type="button" class="btn btn-primary">начоло</button>
+<button type="button" class="btn btn-secondary">победители</button>
+<button type="button" class="btn btn-success">проигравшие</button>
+<button type="button" class="btn btn-danger">награждения</button>
+<button type="button" class="btn btn-warning">Warning</button>
+
+<h1> <span class="badge bg-secondary">областной турнир между 2006-2005 годов по футболу</span></h1>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">номера</th>
+      <th scope="col">название команды</th>
+      <th scope="col">город</th>
+      <th scope="col">забитые голы</th>
+      </thead>
+    </tr>
+
+
+</div>
+</template>
+
+<script>
+
+
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="dsp" >
+<p> капитан америка </p>
+<p> Имя {{fname}} </p>
+  <p> Очество {{lname}} </p>
+  <button type="button" class="btn btn-primary" @click="chengerName">Изменить</button>
+
+
+
+
+  </div>
+</template>
+
+<script>
+
+</style>
